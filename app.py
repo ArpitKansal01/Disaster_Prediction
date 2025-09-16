4bff7265f0a977139f7de0bb8ed192fed1b1c520
ArpitKansal01 <kansalarpit06@gmail.com>
2025-09-16 21:19:05 +0530

    added uvicorn start

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ async def predict(file: UploadFile = File(...)):
 # ----------------------
 # Optional local run
 # ----------------------
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import os, uvicorn
+    port = int(os.environ.get("PORT", 8000))  # <- This line makes Render happy
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
